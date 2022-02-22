f2b2137e4b5be4755007c4100a850e3ffc649f42
Natalia <natinho@yandex.ru>
2022-02-22 23:35:37 +0300

    Added calculations

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,13 @@
-class main {
+val scan = java.util.Scanner(System.`in`)
+fun main() {
+    println("Введите сумму в копейках:")
+    val amount = scan.nextFloat()
+    val commission = if (amount * 0.0075 < 35000) {
+        35000
+    } else {
+        amount * 0.0075
+    }
+    println("Комиссия = $commission копеек")
+
+
 }
